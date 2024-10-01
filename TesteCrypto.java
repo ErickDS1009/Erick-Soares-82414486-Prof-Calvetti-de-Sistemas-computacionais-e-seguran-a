f92cf78678d15acfd4650823518969412f6eab2a
ErickDS1009 <erisoares01@hotmail.com>
2024-10-01 16:41:56 -0300

    Update TesteCrypto.java

--- TesteCrypto.java
+++ TesteCrypto.java
@@ -8,85 +8,85 @@ public class TesteCrypto {
         byte[] bMsgClara = null;
         byte[] bMsgCifrada = null;
         byte[] bMsgDecifrada = null;
-
+        //Instancia objeto da classe Impressora
         Impressora prn = new Impressora();
-
+        //Imprime marcador de bloco
         System.out.println("--------------------------------------------------------------");
-
+        //imprime texto
         System.out.println(">>>imprimindo mensagem original...");
         System.out.println("");
-
+        //Converte o teexto String dado no equivalentet byte []
         bMsgClara = sMsgClara.getBytes("ISO-8859-1");
-
+        //Imprime cabecalho da mensagem
         System.out.println("Mensagem Clara (Hexadecimal):");
-
+        //Imprime o texto original em Hexagonal
         System.out.println(prn.hexBytesToString(bMsgClara));
         System.out.println("");
-
+        //Imprime cabecalho da mensagem
         System.out.println("Mensagem Clara (String):");
-
+        //Imprime o texto original em String
         System.out.println(sMsgClara);
         System.out.println("");
-
-
-
-
+        /*
+         * criptografia Dummy----------------------------------------------------------------
+         */
+        //Imprime texto
         System.out.println(">>>Cifrando com o Algoritmo Dummy...");
         System.out.println("");
-
+        //Instancia um objeto da classe CryptoDummy
         CryptoDummy cdummy = new CryptoDummy();
-
+        //Gera a chave criptografica Dummy simetrica e nome do arquivo onde sera armazenado
         cdummy.geraChave( new File ("chave.dummy"));
-
+        //Gera a chave Dummy da mensagem dada,com a chave Dummy simetrica dada
         cdummy.geraCifra(bMsgClara, new File ("chave.dummy"));
-
+        //Recebe o texto cifrado
         bMsgCifrada = cdummy.getTextoCifrado();
-
+        //Converte o texto byte[] no equivalente String
         sMsgCifrada = (new String (bMsgCifrada, "ISO-8859-1"));
-
+        //Imprime cabecalho da mensagem
         System.out.println("Mensagem Cifrada (Hexadecimal):");
-
+        //Imprime o texto cifrado em Hexadecimal
         System.out.println(prn.hexBytesToString(bMsgCifrada));
         System.out.println("");
-
+        //Imprime cabecalho da mensagem
         System.out.println("Mensagem Cifrada (String):");
-
+        //Imprime o texto cifrado em String
         System.out.println(sMsgCifrada);
         System.out.println("");
-
+        //Imprime texto
         System.out.println(">>>Decifrando com o Algoritmo Dummy");
         System.out.println("");
-
+        //Gera a decifra Dummy da mensagem dada, segundo a chave Dummy simetrica gerada
         cdummy.geraDecifra(bMsgCifrada, new File ("chave.dummy"));
-
+        //Recebe o texto decifrado
         bMsgDecifrada = cdummy.getTextoDecifrado();
-
+        //Converte o texto bity[] no equivalente String
         sMsgDecifrada = (new String (bMsgDecifrada, "ISO-8859-1"));
-
+        //Imprime cabecalho da mensagem
         System.out.println("Mensagem Decifrada (Hexadecimal):");
-
+        //Imprime o texto decifrado em Hexadeciaml
         System.out.println(prn.hexBytesToString(bMsgDecifrada));
         System.out.println();
-
+        //Imprime o cabecalho da mensagem
         System.out.println("Mensagem Decifrada (String):");
-
+        //Imprime o texto decifrado em String
         System.out.println(sMsgDecifrada);
         System.out.println("");
-
-
-
-
+        /*
+        * criptografia Dummy----------------------------------------------------------------
+        */
+        //Imprime o texto
         System.out.println(">>> Cifrando com Algoritmo AES...");
         System.out.println("");
-
+         //Instancia um objeto da classe CriptoAES
         CryptoAES caes = new CryptoAES();
-
+         //Gera a chave Criptografica AES simetrica e nome do arquivo onde sera armazenada
         caes.geraChave( new File ("chave.simetrica"));
-
+        //Gera a cifra AES da mensagem dada, com a chave Dummy simetrica dada
         caes.geraCifra(bMsgClara, new File ("chave.simetrica"));
-
+        //Recebe o texto cifrado
         bMsgCifrada = caes.getTextoCifrado();
-
+        //Converte o texto byte[] no equivalente String
         sMsgCifrada = (new String (bMsgCifrada, "ISO-8859-1"));
         //Imprime cabecalho da mensagem
         System.out.println("Mensagem Cifrada (Hexadecimal) :");
